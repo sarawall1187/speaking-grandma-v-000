e5d15053f30722b849f1be16aa49f373155dfd88
Sara Walker <sarawall1187@gmail.com>
2018-12-09 21:56:46 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,3 +1,28 @@
+#require 'pry'
+def speak_to_grandma(phrase= "I LOVE YOU GRANDMA!")
+ if !phrase.upcase 
+  puts "HUH? SPEAK UP, SONNY!" 
+ elsif phrase.upcase 
+  puts "NO, NOT SINCE 1938!" 
+ elsif phrase.upcase  
+  return "I LOVE YOU TOO PUMPKIN!"
+ end
+end
+
+
+
+
+# def speak_to_grandma(string argument)
+#   check if phrase is written in uppercase?
+#   if it ISNT- return "HUH? SPEAK UP, SONNY!"
+#   else if it IS ALL UPPERCASE- "NO, NOT SINCE 1938!"
+#     if it is EQUAL to "I LOVE YOU GRANDMA!" put(return)I LOVE YOU TOO PUMPKIN!
+# end 
+# end
+
+
+
+
 # Write a speak_to_grandma method.
 
 # Whatever you say to grandma, she should respond with
